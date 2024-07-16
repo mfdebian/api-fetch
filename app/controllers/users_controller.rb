7b551f0b2dfd6db85d7cf42f01f984e9c4acf6cb
Alfredo González <alfredo.arosteguy@gmail.com>
2024-07-15 21:49:19 -0400

    added show user controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def index
     @users = FetchUsersService.call
   end
+
+  def show
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
