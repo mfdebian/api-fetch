8d696e96a774555912b99d6581c1da63724112e6
Alfredo González <alfredo.arosteguy@gmail.com>
2024-07-15 22:18:07 -0400

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,30 @@ class UsersController < ApplicationController
   def show
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to user_url(@user) }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :username, :email, :phone, :website)
     end
 end
