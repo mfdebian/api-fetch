a3b5bfb3f369e155bfb23106e2e7b141f8346a6a
Alfredo González <alfredo.arosteguy@gmail.com>
2024-07-16 21:40:06 -0400

    added update, destroy methods and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = FetchUsersService.call
@@ -23,6 +23,30 @@ class UsersController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_url(@user), notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
-  validates :username, presence: true, length: { minimum: 6, maximum: 25 }, uniqueness: true
+  validates :username, presence: true, length: { minimum: 4, maximum: 25 }, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :phone, presence: true
 end
